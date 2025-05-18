77407e61ceceb6689a598d33405c5762082007fb
Umme Ohida Rahman <ohida.mfs@bau.edu.bd>
2025-05-18 11:55:37 +0600

    Create Practise 18.05.R

---
+++ Firts.R/Practise 18.05.R
@@ -0,0 +1,49 @@
+# data types in R 
+# Numeric - integer / Floating Point
+# String/Character - "Umme Ohida"/ "Ohida"/ 'ohida'
+# Logical 
+
+# Examples of Numeric data types 
+a = 11
+b = 7.19
+class(a)
+class(b)
+
+# String/Character data
+name = 'Umme Ohida'
+name
+class(name)
+
+c = "52"
+print(c + 9)
+print(a + 9)
+class(c)
+
+# Logical data type - TRUE, FALSE (T, F)
+print(a > b)
+a < b
+a > b
+
+x = 19
+y = 22-3
+print(x == y)
+print(x != y)
+print(y >= x)
+
+# Logical Operators in R
+# && logical operator
+# || logical operator
+
+4 == 4 && 5 == 5
+x != y || x == y
+ # simple mathematical operators in R 
+Z = 6 + 9 - 8 * (2 / 3)
+z
+
+w = (4 + 3 - 3) * 2 /3 
+w
+
+y = 4^3
+y
+11 / 5
+11%%5
